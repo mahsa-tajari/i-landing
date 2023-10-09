4f0837cfe86817958ce069d2b08efe69fc17972b
mahsa-tajari <khodemahsatjr@gmail.com>
2023-10-09 13:35:12 +0330

    companies section

--- src/companies/companies.js
+++ src/companies/companies.js
@@ -3,11 +3,14 @@ import './companies.css'
 function Companies() {
     return(
         <section id='companies'>
-            <div className="companiy-logo-wrapper"><img src="./company1.png" alt="company-logo" /></div>
-            <div className="companiy-logo-wrapper"><img src="./company2" alt="company-logo" /></div>
-            <div className="companiy-logo-wrapper"><img src="./company3" alt="company-logo" /></div>
-            <div className="companiy-logo-wrapper"><img src="./company4" alt="company-logo" /></div>
-            <div className="companiy-logo-wrapper"><img src="./company5" alt="company-logo" /></div>
+            <h2>« شرکت هایی که به آی لندینگ اعتماد کردند »</h2>
+            <div className='companies-logos-wrapper'>
+                <div className="companiy-logo-wrapper"><img src="./company1.png" alt="company-logo" /></div>
+                <div className="companiy-logo-wrapper"><img src="./company2.png" alt="company-logo" /></div>
+                <div className="companiy-logo-wrapper"><img src="./company3.png" alt="company-logo" /></div>
+                <div className="companiy-logo-wrapper"><img src="./company4.png" alt="company-logo" /></div>
+                <div className="companiy-logo-wrapper"><img src="./company5.png" alt="company-logo" /></div>
+            </div>
         </section>
     )
 }
