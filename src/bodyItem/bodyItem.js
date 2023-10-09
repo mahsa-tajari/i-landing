c10b9cb4d14c3dbfd7112d925ec38c33a614fbad
mahsa-tajari <khodemahsatjr@gmail.com>
2023-10-09 16:51:51 +0330

    edit layout

--- src/bodyItem/bodyItem.js
+++ src/bodyItem/bodyItem.js
@@ -1,16 +1,16 @@
 import './bodyItem.css'
 
-function BodyItem(props){
+function BodyItem({direction,imgSrc,shortTitle,title,theme}){
     return(
-        <div className='body-item' id={props.direction}>
+        <div className='body-item' id={direction}>
             <div className='body-item-img-wrapper'>
-                <img src={props.imgSrc} alt="picture" />
+                <img src={imgSrc} alt="picture" />
             </div>
             <div className='body-item-desc'>
-                <h2 className='body-item-short-title'>{props.shortTitle}</h2>
-                <h2 className='body-item-title'>{props.title}</h2>
+                <h2 className='body-item-short-title'>{shortTitle}</h2>
+                <h2 className='body-item-title'>{title}</h2>
                 <p className='body-item-text'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
-                <a className={props.theme} href="#">بیشتر بدانید</a>
+                <a className={theme} href="#">بیشتر بدانید</a>
             </div>
         </div>
     )
