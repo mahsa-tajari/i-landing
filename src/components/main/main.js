6d7ba3426ba839c02cb9b99801aa99c1352f5983
mahsa-tajari <khodemahsatjr@gmail.com>
2023-10-15 07:55:25 +0330

    edit main structure and styles

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,6 +1,6 @@
 import './main.css'
 import Companies from '../companies/companies'
-import BodyItem from '../bodyItem/bodyItem'
+import Advantages from '../advantages/advantages'
 import OurServices from '../our-services/our-services'
 import Plans from '../plans/plans'
 import Questions from '../questions/questions'
@@ -12,19 +12,10 @@ import Buy from '../buy/buy'
 import Contact from '../contact-us/contact'
 import Footer from '../footer/footer'
 function Main() {
-    const bodyItemDta = [
-        {shortTitle: 'طراحی Mobile-first',imgSrc: './images/main/picture1.svg', direction: 'right', theme: 'default',title: 'ریسپانسیو برای تمامی دستگاه ها'},
-        {shortTitle: 'آناتومیِ استاندارد',imgSrc:'./images/main/picture2.svg',direction:'left', theme: 'blue-theme', title: 'طراحی و توسعه با رعایت اصول جهانی'},
-        {shortTitle: 'رشد کسب و کار',imgSrc:'./images/main/picture3.svg',direction:'right', theme: 'default', title: 'تبدیل بازدید کننده ساده به مشتری!'},
-    ]
     return(
         <main>
             <Companies></Companies>
-            <div className='body-wrapper'>
-            <BodyItem {...bodyItemDta[0]}></BodyItem>
-            <BodyItem {...bodyItemDta[1]}></BodyItem>
-            <BodyItem {...bodyItemDta[2]}></BodyItem>
-            </div>
+            <Advantages></Advantages>
             <OurServices></OurServices>
             <Plans></Plans>
             <Questions></Questions>
