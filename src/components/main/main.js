4df3a4d8b15c9b93b8dc0dd074890959145c46f4
mahsa-tajari <khodemahsatjr@gmail.com>
2023-10-15 07:49:43 +0330

    edit main component

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,9 +1,8 @@
 import './main.css'
 import Companies from '../companies/companies'
 import BodyItem from '../bodyItem/bodyItem'
-import Title from '../title/title'
 import OurServices from '../our-services/our-services'
-import Cart from '../cart/cart'
+import Plans from '../plans/plans'
 import Questions from '../questions/questions'
 import Achievements from '../achievements/achievements'
 import Comments from '../comments/comments'
@@ -18,11 +17,6 @@ function Main() {
         {shortTitle: 'آناتومیِ استاندارد',imgSrc:'./images/main/picture2.svg',direction:'left', theme: 'blue-theme', title: 'طراحی و توسعه با رعایت اصول جهانی'},
         {shortTitle: 'رشد کسب و کار',imgSrc:'./images/main/picture3.svg',direction:'right', theme: 'default', title: 'تبدیل بازدید کننده ساده به مشتری!'},
     ]
-    const cartData = [
-        {planName: "ساده", price: "99", yearlyPrice: "1 میلیون ", liValue: "اول", style: "normal"},
-        {planName: "حرفه‌ای", price: "199", yearlyPrice: "2 میلیون ", style: "active"},
-        {planName: "نامحدود",  price: "399",  yearlyPrice: "4 میلیون ",  style: "normal"}
-    ]
     return(
         <main>
             <Companies></Companies>
@@ -32,14 +26,7 @@ function Main() {
             <BodyItem {...bodyItemDta[2]}></BodyItem>
             </div>
             <OurServices></OurServices>
-            <section className='plans'>
-            <Title title="پلن‌های ما"></Title>
-            <div className='plans-wrapper'>
-                <Cart {...cartData[0]}></Cart>
-                <Cart {...cartData[1]}></Cart>
-                <Cart {...cartData[2]}></Cart>
-            </div>
-            </section>
+            <Plans></Plans>
             <Questions></Questions>
             <Achievements></Achievements>
             <Comments></Comments>
